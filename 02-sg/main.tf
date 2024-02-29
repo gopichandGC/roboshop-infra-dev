67d6791667d3d74f9d4c3ca43e7209cc3c6df209
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-29 10:29:49 +0530

    Security Groups Creation For Roboshop Project

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -116,6 +116,18 @@ module "app_alb" {
   sg_name        = "app-alb"
   #sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
+
+# App ALB should accept connections only from VPN, since it is internal
+resource "aws_security_group_rule" "app_alb_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+
 #openvpn
 resource "aws_security_group_rule" "vpn_home" {
   security_group_id = module.vpn.sg_id
