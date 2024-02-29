f2ba26851561e677a6f7bfe1fd186fef33f694b6
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-02-29 11:12:05 +0530

    Configuring Catalogue Component

--- 06-catalogue/data.tf
+++ 06-catalogue/data.tf
@@ -1,3 +1,23 @@
+data "aws_ami" "centos8"{
+    owners = ["973714476881"]
+    most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
 data "aws_ssm_parameter" "app_alb_sg_id" {
   name = "/${var.project_name}/${var.environment}/app_alb_sg_id"
 }
