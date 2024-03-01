dc03b57f8d5ff38bb9a34d4dc43c58f14fe43aa0
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-03-01 18:49:55 +0530

    Configuring WEB-ALB For Roboshop-Infra

--- 08-web-alb/main.tf
+++ 08-web-alb/main.tf
@@ -12,3 +12,21 @@ resource "aws_lb" "web_alb" {
     var.tags
   )
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_ssm_parameter.acm_certificate_arn.value
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "This is from WEB ALB using HTTPS"
+      status_code  = "200"
+    }
+  }
+}
