fea5cabcce1f654355e44c668b0a7ddad1cc12d8
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-03-01 19:26:47 +0530

    Configuring Web Component

--- 09-web/main.tf
+++ 09-web/main.tf
@@ -155,3 +155,16 @@ resource "aws_lb_listener_rule" "web" {
   }
 }
 
+resource "aws_autoscaling_policy" "web" {
+  autoscaling_group_name = aws_autoscaling_group.web.name
+  name                   = "${local.name}-${var.tags.Component}"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 5.0
+  }
+}
