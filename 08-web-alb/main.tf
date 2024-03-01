89592809690b342a4b84470926f97cc6c20452cd
gopichandGC <gopichandvadlamudi123@gmail.com>
2024-03-01 18:50:37 +0530

    Configuring WEB-ALB For Roboshop-Infra

--- 08-web-alb/main.tf
+++ 08-web-alb/main.tf
@@ -30,3 +30,20 @@ resource "aws_lb_listener" "https" {
     }
   }
 }
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "web-${var.environment}"
+      type    = "A"
+      alias   = {
+        name    = aws_lb.web_alb.dns_name
+        zone_id = aws_lb.web_alb.zone_id
+      }
+    }
+  ]
+}
